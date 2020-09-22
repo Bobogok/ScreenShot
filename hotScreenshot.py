e590dce748510a4bb495ae2cb4c9032561531973
Ivan M <40141225+Bobogok@users.noreply.github.com>
2020-09-22 14:30:59 +0400

    add path and make better imports

--- hotScreenshot.py
+++ hotScreenshot.py
@@ -1,14 +1,18 @@
 #! python3
 # hotScreenshot.py - программа, позволяющая делать моментальный скрин и сохранять его на рабочем столе
 
-import pyautogui, os, keyboard, time
+import pyautogui
+import keyboard 
+import time
+import os 
 
-nameDir = 'C:\\Users\\i7-2\\Desktop'
- 
-os.chdir(nameDir)
+# path to save on Desktop
+path = '~\\Desktop'
+fullPath = os.path.expanduser(path)
+os.chdir(fullPath)
 
+# take a screenShot combination of keys ctrl+`
 count = 0
-
 while True:
     time.sleep(0.05)
     try:
